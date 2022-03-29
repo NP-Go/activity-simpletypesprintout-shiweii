65cca894376529d73246e8178f5f8302ecfdc7eb
Ou Shiwei <shiwei.ou@protonmail.com>
2022-03-29 13:20:47 +0800

    Add and print types of variables

--- typesInGo.go
+++ typesInGo.go
@@ -1,9 +1,25 @@
 package main
 
-//Insert variables declarations here based on activity
+import (
+	"fmt"
+)
 
 func tellMeTypes() {
+
+	//Insert variables declarations here based on activity
+	text := "The following is the account information."
+	firstName := "Luke"
+	lastName := "Skywalkter"
+	age := 20
+	weight := 73.0
+	height := 1.72
+	credits := 123.55
+	accountName := "admin"
+	accountPw := "password"
+	subscribed := true
+
 	//insert code here to print out types of variables
+	fmt.Printf("%T %T %T %T %T %T %T %T %T %T\n", text, firstName, lastName, age, weight, height, credits, accountName, accountPw, subscribed)
 }
 
 func main() {
